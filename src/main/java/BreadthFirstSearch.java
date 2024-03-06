43d89814cd2cf082950867ead04776f041727c24
arjafree <107591831+arjafree@users.noreply.github.com>
2024-03-06 08:25:48 +0800

    Done

--- src/main/java/BreadthFirstSearch.java
+++ src/main/java/BreadthFirstSearch.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch {
 
@@ -9,35 +11,29 @@ public class BreadthFirstSearch {
     public static int[] bfs(int[][] graph) {
 
         // Create a Queue and add the first node (0) to it. Use your Queue class that you just created!
-
+        Queue<Integer> queue = new LinkedList<>();
         // Create a visited array. This array will track whether we have visited a specific node.
-
+        boolean[] visited = new boolean[graph.length];
         // Create a bfs path array and a bfs path index. This array will track the order that the nodes were visited.
-
+        int[] path = new int[graph.length];
+        int pathIndex = 0;
         // Add the first node (0) to the bfs array.
-
+        queue.add(0);
         // Record the first node (0) as visited.
-        
+        visited[0] = true;
         // Time to traverse the graph! While the queue is not empty ...
-
-            // Dequeue (poll) the queue and store this value in a variable called currentNode.
-
-            // Record this node as visited.
-
-            // Add this node to the path and update the path index.
-
-            // Obtain an array of this node's neighbouring/adjacent nodes.
-        
-            // Explore the current nodes neighbouring nodes. For each neighbouring node ...
-
-                // If a neighbour hasn't been visited before ...
-
-                    // Record the neighbour as visited.
-        
-                    // Add the neighbour to the queue. 
-
-        // Return bfs path.
-        
-        return null;
+        while(!queue.isEmpty()){
+            int currentNode = queue.poll();
+            visited[currentNode] = true;
+            path[pathIndex++] = currentNode;
+            int[] neighbors = graph[currentNode];
+            for (int neighbor : neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return path;
     }
 }
